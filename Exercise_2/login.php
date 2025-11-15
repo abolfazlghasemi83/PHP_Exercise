7c06e0f6a0e06df1824ec19b44b6bab57e4f571d
abolfazl ghasemi <125478662+abolfazlghasemi83@users.noreply.github.com>
2025-11-15 23:47:13 +0330

    add login.php

---
+++ Exercise_2/login.php
@@ -0,0 +1,60 @@
+<center style="color:blue ; box-shadow:1px 20px 50px aqua"> 
+    <span>
+    <h2><b>Login Form</b></h2>
+
+    <form action="home.php" method="get">
+        <input name="firstname" type="text" placeholder="Enter your first name" >
+        <input type="color" name="color1">
+        <br><br>
+
+        <input name="lastname" type="text" placeholder="Enter your last name" >
+        <input type="color" name="color2">
+
+        <br><br>
+
+        <input name="age" type="number" placeholder="Enter your age" >
+        <input type="color" name="color3">
+
+        <br><br>
+
+        <input name="nationalcode" type="text" placeholder="Enter your national code" >
+        <input type="color" name="color4">
+
+        <br><br>
+
+        <input name="address" type="text" placeholder="Enter your address" >
+        <input type="color" name="color5">
+
+        <br><br>
+
+        <input name="email" type="email" placeholder="Enter your email" >
+        <input type="color" name="color6">
+
+        <br><br>
+
+        <input name="phone" type="text" placeholder="Enter your phone number" >
+        <input type="color" name="color7">
+
+        <br><br>
+
+        <input name="username" type="text" placeholder="Enter your username" >
+        <input type="color" name="color8">
+
+        <br><br>
+
+        <input name="password" type="password" placeholder="Enter your password" >
+        <input type="color" name="color9">
+
+        <br><br>
+
+        <input name="city" type="text" placeholder="Enter your city" >
+        <input type="color" name="color10">
+
+        <br><br>
+
+        <input type="submit" value="SEND">
+    </form>
+    </span>
+</center>
+
+
